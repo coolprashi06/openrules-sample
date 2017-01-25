a4280af081c40e564ace06e8a5599d931c5696bb
Prashast Saxena <coolprashi06@gmail.com>
2017-01-26 00:55:15 +0530

    refactor code

--- src/main/java/com/prashast/Main.java
+++ src/main/java/com/prashast/Main.java
@@ -3,10 +3,7 @@ package com.prashast;
 public class Main {
 
     public static void main(String args[]) throws Exception{
-
-        PurchaseOrderDecision pod = new PurchaseOrderDecision();
-        System.out.println(pod.getEligibility("NL",8));
-
+        
         CustomerGreetingDecision cgd = new CustomerGreetingDecision();
         System.out.println(cgd.getGreetingMessage("Prashast",9,"Male","Single"));
     }
